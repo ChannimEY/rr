a090b19e413ea6a62c3cc30b189e8c15d52c8fa7
ChannimEy <channimey1@gmail.com>
2024-08-30 14:58:22 +0700

    first push

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<thread>
+using namespace std;
+using namespace std::this_thread;
+using namespace std::chrono_literals;
+int main()
+{
+  cout<<"Hello World!";
+  return 0;
+}
